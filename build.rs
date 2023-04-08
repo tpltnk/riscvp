594d635e1d5adb611f4fec4a9c6ac9c62038aef6
tpltnk <33201077+tpltnk@users.noreply.github.com>
2023-04-08 20:13:27 +0200

    build qemu and riscv targets

--- build.rs
+++ build.rs
@@ -1,12 +1,18 @@
-use std::{fs, path::PathBuf};
+use std::{
+    fs,
+    path::PathBuf,
+    process::{Command, ExitCode, Stdio},
+};
 
 use cc::Build;
+
+use std::env;
 
 const OUT_DIRX: &str = env!("OUT_DIRX");
 const IN_DIR: &str = env!("IN_DIR");
 const INC_DIR: &str = env!("INC_DIR");
 
-fn main() {
+fn main() -> ExitCode {
     if PathBuf::from(OUT_DIRX).exists() {
         fs::remove_dir_all(OUT_DIRX).expect("failed to remove directory");
     }
@@ -26,22 +32,26 @@ fn main() {
         })
         .for_each(|maybe_entry| {
             let entry = maybe_entry.unwrap();
+            let mut build = Build::new();
+            if build.get_compiler().is_like_clang() {
+                println!("cargo:warning=clang detected");
+                build.flag("-E").flag("-P");
+            }
+            if build.get_compiler().is_like_gnu() {
+                println!("cargo:warning=gcc detected");
+                build.flag("-E").flag("-P");
+            }
+            if build.get_compiler().is_like_msvc() {
+                println!("cargo:warning=msvc detected");
+                build.flag("/E").flag("/P");
+            }
+            let compiler = build
+                .include(INC_DIR)
+                .define("RISCVP_AUTHORS", env!("CARGO_PKG_AUTHORS"))
+                .get_compiler();
             fs::write(
                 PathBuf::from(OUT_DIRX).join(entry.path().file_name().unwrap()),
-                Build::new()
-                    .flag(if cfg!(target_os = "windows") {
-                        "/E"
-                    } else {
-                        "-E"
-                    })
-                    .flag(if cfg!(target_os = "windows") {
-                        "/C"
-                    } else {
-                        "-C"
-                    })
-                    .include(INC_DIR)
-                    .define("RISCVP_AUTHORS", env!("CARGO_PKG_AUTHORS"))
-                    .get_compiler()
+                compiler
                     .to_command()
                     .arg(entry.path())
                     .output()
@@ -59,6 +69,13 @@ fn main() {
         });
 
     add_watchers();
+    if cfg!(target_os = "windows") {
+        println!("cargo:warning=qemu on windows not supported yet");
+        ExitCode::FAILURE
+    } else {
+        build_qemu();
+        ExitCode::SUCCESS
+    }
 }
 
 fn add_watchers() {
@@ -87,4 +104,26 @@ fn add_watchers() {
     println!("cargo:rerun-if-env-changed=INC_DIR");
     println!("cargo:rustc-link-arg=-nostartfiles");
     println!("cargo:OUT_DIRX={OUT_DIRX}");
+}
+
+fn build_qemu() {
+    if !PathBuf::from("build").exists() {
+        fs::create_dir("build").unwrap();
+        env::set_current_dir("build").unwrap();
+        println!("cargo:warning=configuring qemu");
+        Command::new("../qemu/configure")
+            .arg("--target-list=riscv64-softmmu")
+            // .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .status()
+            .expect("Failed to execute process");
+        println!("cargo:warning=building qemu");
+        Command::new("make")
+            .arg("-j")
+            .arg(format!("{}", num_cpus::get() / 2))
+            // .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit()) // stderr available at target/<profile>/build/riscvp-<hash>/stderr
+            .status()
+            .expect("Failed to execute process");
+    }
 }
